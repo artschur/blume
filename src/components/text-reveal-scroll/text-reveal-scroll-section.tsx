2287365905c94b7816e8488db3936ed0d293645c
Arthur Schurhaus <arthurschur@icloud.com>
2025-04-04 11:19:37 -0300

    added text-reveal on scroll section

---
+++ src/components/text-reveal-scroll/text-reveal-scroll-section.tsx
@@ -0,0 +1,5 @@
+import { TextReveal } from "../magicui/text-reveal";
+
+export function TextRevealSection() {
+    return <TextReveal>A blume vai mudar o jeito que os clientes interagem com sua empresa.</TextReveal>;
+}
